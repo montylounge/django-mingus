dc99874c3773504377bd0570dbbf741fe5722816
kevin fricovsky <kevin@montylounge.com>
2009-08-23 23:09:44 -0400

    added new basic_blog settings

--- mingus/settings.py
+++ mingus/settings.py
@@ -41,6 +41,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 "django.core.context_processors.debug",
 "django.core.context_processors.i18n",
 "django.core.context_processors.media",
+"basic.blog.context_processors.blog_settings",
 "mingus.core.context_processors.site_info",
 "navbar.context_processors.navbars",
 )
