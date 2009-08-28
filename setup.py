9e9988057e9e40ee071d96fe11ec84aff3560646
kevin fricovsky <kevin@montylounge.com>
2009-08-27 23:25:24 -0400

    bumped version and updated url

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
  
 setup(
     name='django-mingus',
-    version='0.1.2',
+    version='0.1.8',
     description='A django blog engine.',
     author='Kevin Fricovsky',
     author_email='kfricovsky@gmail.com',
-    url='http://bitbucket.org/montylounge/django-mingus/',
+    url='http://github.com/montylounge/django-mingus/tree',
     packages=['mingus'],
 )
  
