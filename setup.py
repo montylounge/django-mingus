efa5f0693a25c44419df1679244a36df62c33bc0
kevin fricovsky <kevin@montylounge.com>
2009-09-04 01:55:43 -0400

    labeled 0.2 release

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
- 
+
 setup(
     name='django-mingus',
-    version='0.1.9',
+    version='0.2',
     description='A django blog engine.',
     author='Kevin Fricovsky',
     author_email='kfricovsky@gmail.com',
     url='http://github.com/montylounge/django-mingus/tree',
     packages=['mingus'],
 )
- 
+
