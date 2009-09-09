88c744e086dd019716bf4b82e56fc41871d0aca6
kevin fricovsky <kevin@montylounge.com>
2009-09-09 13:12:50 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-mingus',
-    version='0.4.4',
+    version='0.4.5',
     description='A django blog engine.',
     author='Kevin Fricovsky',
     author_email='kfricovsky@gmail.com',
