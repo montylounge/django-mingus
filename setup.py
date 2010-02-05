8961752e12f8596beed3d1edaefe28abd8223aee
kevin fricovsky <kevin@montylounge.com>
2010-02-05 08:52:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='django-mingus',
-    version='0.9.4',
+    version='0.9.5',
     description='A django blog engine.',
     long_description=read('README.textile'),
     author='Kevin Fricovsky',
