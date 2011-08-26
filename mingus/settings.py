7006e8e95cafda5df2831889b910831d4cc47a77
Kevin Tonon <kevin@betweenconcepts.com>
2011-08-25 21:30:16 -0400

    Enabled markup app.

--- mingus/settings.py
+++ mingus/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = (
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   'django.contrib.sites',
+  'django.contrib.markup',
   'django.contrib.messages',
   'django.contrib.staticfiles',
   'django.contrib.admin',
